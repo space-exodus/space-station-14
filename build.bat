599a997ae163b6ffe5f61f86560191e35d6d5fbd
Lokilife <lokilife0002@gmail.com>
2024-08-30 14:15:17 +0300

    Update secrets prototypes when loading secrets module

--- build.bat
+++ build.bat
@@ -2,6 +2,13 @@
 set configuration=%1
 
 if exist Secrets (
+    rm -r Resources/Prototypes/ExodusSecrets
+    rm -r Resources/Locale/ru-RU/exodus-secrets
+    rm -r Resources/Textures/ExodusSecrets
+
+    cp -R Secrets/Resources/Prototypes Resources/Prototypes/ExodusSecrets
+    cp -R Secrets/Resources/Locale Resources/Locale/ru-RU/exodus-secrets
+    cp -R Secrets/Resources/Textures Resources/Textures/ExodusSecrets
     dotnet build -c %configuration% Secrets
 ) else (
     dotnet build -c %configuration%
--- runserver-bin.bat
+++ runserver-bin.bat
@@ -1,2 +1,11 @@
 @echo off
+if exist Secrets (
+    rm -r Resources/Prototypes/ExodusSecrets
+    rm -r Resources/Locale/ru-RU/exodus-secrets
+    rm -r Resources/Textures/ExodusSecrets
+
+    cp -R Secrets/Resources/Prototypes Resources/Prototypes/ExodusSecrets
+    cp -R Secrets/Resources/Locale Resources/Locale/ru-RU/exodus-secrets
+    cp -R Secrets/Resources/Textures Resources/Textures/ExodusSecrets
+)
 start ./bin/Content.Server/Content.Server.exe
