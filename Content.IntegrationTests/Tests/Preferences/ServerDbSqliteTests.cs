897f6906b36498e1e18675422a0a5ce774a95963
Maxim <60302610+Lokilife@users.noreply.github.com>
2024-09-22 00:01:55 +0300

    Fix voice in server db sqlite test (#83)

--- Content.IntegrationTests/Tests/Preferences/ServerDbSqliteTests.cs
+++ Content.IntegrationTests/Tests/Preferences/ServerDbSqliteTests.cs
@@ -49,7 +49,7 @@ namespace Content.IntegrationTests.Tests.Preferences
                 Name = "Charlie Charlieson",
                 FlavorText = "The biggest boy around.",
                 Species = "Human",
-                Voice = "Eugene", // Corvax-TTS
+                Voice = "Illidan", // Corvax-TTS
                 Age = 21,
                 Appearance = new(
                     "Afro",
