aaf0c261b9131fe9544f0563d6ab67d3f103775a
Fragoler <e.f.oleg@mail.ru>
2025-01-05 10:40:03 +0300

    fuck coderabbit

--- build.bat
+++ build.bat
@@ -5,7 +5,7 @@ if exist Secrets (
     rm -r Resources/Prototypes/ExodusSecrets
     rm -r Resources/Locale/ru-RU/exodus-secrets
     rm -r Resources/Textures/ExodusSecrets
-    rm -r Secrets/Resources/Audio
+    rm -r Resources/Audio/ExodusSecrets
 
     cp -R Secrets/Resources/Prototypes Resources/Prototypes/ExodusSecrets
     cp -R Secrets/Resources/Locale Resources/Locale/ru-RU/exodus-secrets
