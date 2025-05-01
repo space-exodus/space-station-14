078aca49e26f25aca85cf6592d57648a7bfcb3f1
Lokilife <lokilife0002@gmail.com>
2025-05-01 05:12:46 +0300

    Mineral Resonance

---
+++ Content.Shared/_Exodus/Traits/Species/MineralResonance/MineralResonanceComponent.cs
@@ -0,0 +1,30 @@
+// © Space Exodus, An EULA/CLA with a hosting restriction, full text: https://raw.githubusercontent.com/space-exodus/space-station-14/master/CLA.txt
+
+using Content.Shared.Actions;
+using Content.Shared.Chat.Prototypes;
+using Robust.Shared.Prototypes;
+
+namespace Content.Shared.Exodus.Traits.Species.MineralResonance;
+
+public sealed partial class MineralResonanceComponent : Component
+{
+    [DataField]
+    public EntProtoId ActionPrototype = "ActionMineralResonance";
+
+    [DataField]
+    public EntityUid? ActionEntity;
+
+    [DataField]
+    public ProtoId<EmotePrototype> TriggerEmote = "JawsSnap";
+
+    [DataField]
+    public float ViewRange = 5f;
+
+    [DataField]
+    public TimeSpan Delay = TimeSpan.FromSeconds(3.5f);
+}
+
+public sealed partial class MineralResonanceUseEvent : InstantActionEvent
+{
+
+}
