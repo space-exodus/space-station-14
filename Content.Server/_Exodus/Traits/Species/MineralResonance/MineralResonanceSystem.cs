c6d0a65dacbe95121b65424e79d0c102c08e0651
Lokilife <lokilife0002@gmail.com>
2025-05-09 19:01:44 +0300

    Rewrite bioluminescence

--- Content.Server/_Exodus/Traits/Species/MineralResonance/MineralResonanceSystem.cs
+++ Content.Server/_Exodus/Traits/Species/MineralResonance/MineralResonanceSystem.cs
@@ -1,3 +1,5 @@
+// © Space Exodus, An EULA/CLA with a hosting restriction, full text: https://raw.githubusercontent.com/space-exodus/space-station-14/master/CLA.txt
+
 using Content.Server.Actions;
 using Content.Server.Chat.Systems;
 using Content.Shared.Exodus.Mining;
