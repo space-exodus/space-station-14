bab5325327df730535e6b6af630a6bdb9a089b55
DarkBanOne <darkbanone0@gmail.com>
2025-08-13 15:17:41 +0300

    aaa

--- Content.Server/_Exodus/CardboardBox/StealthCardboardBoxSystem.cs
+++ Content.Server/_Exodus/CardboardBox/StealthCardboardBoxSystem.cs
@@ -1,6 +1,5 @@
 // © Space Exodus, An EULA/CLA with a hosting restriction, full text: https://raw.githubusercontent.com/space-exodus/space-station-14/master/CLA.txt
 
-
 using Content.Shared.CardboardBox.Components;
 using Content.Shared.Exodus.CardboardBox.Components;
 using Content.Shared.Exodus.Stealth.Components;
@@ -12,35 +11,14 @@ namespace Content.Server.Exodus.CardboardBox;
 
 public sealed class StealthCardboardBoxSystem : EntitySystem
 {
-    [Dependency] private readonly SharedStealthSystem _stealth = default!;
+    [Dependency] private readonly InstantStealthSystem _instantStealth = default!;
 
     public override void Initialize()
     {
         base.Initialize();
 
-        SubscribeLocalEvent<StealthCardboardBoxComponent, ComponentInit>(OnInit);
-        SubscribeLocalEvent<StealthCardboardBoxComponent, ComponentShutdown>(OnShutdown);
         SubscribeLocalEvent<CardboardBoxComponent, StorageAfterOpenEvent>(AfterStorageOpen);
         SubscribeLocalEvent<CardboardBoxComponent, StorageAfterCloseEvent>(AfterStorageClosed);
-    }
-
-    private void OnInit(EntityUid uid, StealthCardboardBoxComponent component, ComponentInit args)
-    {
-        if (!TryComp<CardboardBoxComponent>(uid, out var box))
-            return;
-
-        if (TryComp<EntityStorageComponent>(uid, out var storage) && storage.Open)
-            return;
-
-        _stealth.RequestStealth(uid, nameof(StealthCardboardBoxSystem), component.Stealth);
-    }
-
-    private void OnShutdown(EntityUid uid, StealthCardboardBoxComponent component, ComponentShutdown args)
-    {
-        if (!TryComp<CardboardBoxComponent>(uid, out var box))
-            return;
-
-        _stealth.RemoveRequest(nameof(StealthCardboardBoxSystem), uid);
     }
 
     private void AfterStorageOpen(EntityUid uid, CardboardBoxComponent component, ref StorageAfterOpenEvent args)
@@ -48,15 +26,15 @@ public sealed class StealthCardboardBoxSystem : EntitySystem
         if (!HasComp<StealthCardboardBoxComponent>(uid))
             return;
 
-        _stealth.RemoveRequest(nameof(StealthCardboardBoxSystem), uid);
+        _instantStealth.SetEnabled(uid, false);
     }
 
     private void AfterStorageClosed(EntityUid uid, CardboardBoxComponent component, ref StorageAfterCloseEvent args)
     {
-        if (!TryComp<StealthCardboardBoxComponent>(uid, out var stealthBox))
+        if (!HasComp<StealthCardboardBoxComponent>(uid))
             return;
 
-        _stealth.RequestStealth(uid, nameof(StealthCardboardBoxSystem), stealthBox.Stealth);
+        _instantStealth.SetEnabled(uid, true);
     }
 }
 
--- Content.Shared/_Exodus/CardboardBox/StealthCardboardBoxComponent.cs
+++ Content.Shared/_Exodus/CardboardBox/StealthCardboardBoxComponent.cs
@@ -1,13 +1,6 @@
 // © Space Exodus, An EULA/CLA with a hosting restriction, full text: https://raw.githubusercontent.com/space-exodus/space-station-14/master/CLA.txt
 
-
-using Content.Shared.Exodus.Stealth.Components;
-
 namespace Content.Shared.Exodus.CardboardBox.Components;
 
 [RegisterComponent]
-public sealed partial class StealthCardboardBoxComponent : Component
-{
-    [DataField("stealth")]
-    public StealthData Stealth = new();
-}
+public sealed partial class StealthCardboardBoxComponent : Component {}
